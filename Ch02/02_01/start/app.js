425bcc7224654d3a32f1d15b8f496c08688236ed
Mauricio Feldman-Abe <mauabe@gmx.com>
2019-04-07 12:42:42 -0700

    added babel to hplus project

--- Ch02/02_01/start/app.js
+++ Ch02/02_01/start/app.js
@@ -1,6 +1,19 @@
 (function() {
   "use strict";
 
-  // Start here
+  function ProductImage(props){
+    <img src="../../../assets/red.jpg" alt="" />
+    retun React.createElement('img', {src="../../../assets/red.jpg", alt='Product image' })
+  }
+
+  function ProductCustomizer(props){
+    return React.createElement(
+      'div', 
+      {className: 'customizer'}, 
+      React.createElement('div', { className: 'product-image'}, React.createElement(ProductImage) )
+    );
+  }
+
+  reactDOM.render(React.createElement(ProductCustomizer), document.getElementById('react-root');
 
 })();
--- Ch02/wip/start/app.js
+++ Ch02/wip/start/app.js
@@ -1,6 +1,20 @@
 (function() {
   "use strict";
 
-  // Start here
+  function ProductImage(props){
+    return <img src="../../../assets/red.jpg" alt="Product image" />;
+  }
+
+  function ProductCustomizer(props){
+    return (
+      <div className="customizer">
+        <div className="product-images">
+          <ProductImage />
+        </div>
+      </div>
+    );
+  }
+
+  reactDOM.render(<ProductCustomizer />, document.getElementById('react-root');
 
 })();
