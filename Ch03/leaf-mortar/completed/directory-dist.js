ddea068ab03294e67fc9379ef3771e22cdce5c8b
Mauricio <mauabe@gmx.com>
2019-04-15 22:39:40 -0700

    added animation with React Transition Group

--- Ch03/leaf-mortar/completed/directory-dist.js
+++ Ch03/leaf-mortar/completed/directory-dist.js
@@ -2,8 +2,6 @@
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
@@ -22,19 +20,13 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
         null,
         props.person.name,
         ", ",
-        props.person.title
+        props.person.title,
+        " "
       ),
       React.createElement(
         "p",
         null,
-        React.createElement("img", {
-          className: "size-medium alignright",
-          src: props.person.img,
-          alt: props.person.name,
-          width: "300",
-          height: "300",
-          sizes: "(max-width: 300px) 100vw, 300px"
-        }),
+        React.createElement("img", { className: " size-medium alignright", src: props.person.img, alt: props.person.img, width: "300", height: "300", sizes: "(max-width: 300px) 100vw, 300px" }),
         props.person.bio
       )
     );
@@ -44,25 +36,46 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
     return React.createElement(
       "div",
       { className: "results" },
-      props.people.map(function (person) {
-        return React.createElement(Person, { key: person.id, person: person });
-      })
+      React.createElement(
+        ReactTransitionGroup.TransitionGroup,
+        null,
+        props.people.map(function (person) {
+          return React.createElement(
+            ReactTransitionGroup.CSSTransition,
+            {
+              key: person.id,
+              classNames: {
+                enter: "animated",
+                enterActive: "zoomIn",
+                exit: "animated",
+                exitActive: "zoomOut"
+              },
+              timeout: 1000 },
+            React.createElement(Person, { person: person })
+          );
+        })
+      )
     );
   }
 
   function Filters(props) {
     var titles = window.LMDirectory.titles;
 
-    function updateName(evt) {
-      props.updateFormState("currentName", evt.target.value);
+    function updateName(event) {
+      props.updateFormState({ currentName: event.target.value });
     }
-
-    function updateTitle(evt) {
-      props.updateFormState("currentTitle", evt.target.value);
+    function updateTitle(event) {
+      props.updateFormState({ currentTitle: event.target.value });
     }
-
-    function updateIntern(evt) {
-      props.updateFormState("isIntern", evt.target.checked);
+    function updateIntern(event) {
+      props.updateFormState({ isIntern: event.target.checked });
+    }
+    function updateReset() {
+      props.updateFormState({
+        currentName: '',
+        currentTitle: '',
+        isIntern: false
+      });
     }
 
     return React.createElement(
@@ -76,14 +89,7 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
           { htmlFor: "person-name" },
           "Name:"
         ),
-        React.createElement("input", {
-          type: "text",
-          name: "person_name",
-          placeholder: "Name of employee",
-          id: "person-name",
-          value: props.currentName,
-          onChange: updateName
-        })
+        React.createElement("input", { type: "text", name: "person_name", placeholder: "Name of employee", id: "person-name", value: props.currentName, onChange: updateName })
       ),
       React.createElement(
         "div",
@@ -95,11 +101,7 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
         ),
         React.createElement(
           "select",
-          {
-            name: "person_title",
-            id: "person-title",
-            value: props.currentTitle,
-            onChange: updateTitle },
+          { name: "person_title", id: "person-title", value: props.currentTitle, onChange: updateTitle },
           React.createElement(
             "option",
             { value: "" },
@@ -120,16 +122,14 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
         React.createElement(
           "label",
           null,
-          React.createElement("input", {
-            type: "checkbox",
-            value: "1",
-            name: "person_intern",
-            checked: props.isIntern,
-            onChange: updateIntern
-          }),
-          " ",
-          "Intern"
+          React.createElement("input", { type: "checkbox", value: "1", name: "person_intern", checked: props.isIntern, onChange: updateIntern }),
+          " Intern"
         )
+      ),
+      React.createElement(
+        "div",
+        { className: "group" },
+        React.createElement("input", { type: "reset", value: "Reset", onClick: updateReset })
       )
     );
   }
@@ -148,26 +148,24 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
         currentTitle: "",
         isIntern: false
       };
-
       _this.updateFormState = _this.updateFormState.bind(_this);
       return _this;
     }
 
     _createClass(Directory, [{
       key: "updateFormState",
-      value: function updateFormState(name, val) {
-        this.setState(_defineProperty({}, name, val), this.updatePeopleList);
+      value: function updateFormState(spec) {
+        this.setState(spec, this.updatePeopleList);
       }
-
-      // search the whole employee list with current filters
-
     }, {
       key: "updatePeopleList",
+
+
+      // search the whole employee list with current filters
       value: function updatePeopleList() {
         var filteredPeople = window.LMDirectory.people.filter(function (person) {
           return person.intern === this.state.isIntern && (this.state.currentName === "" || person.name.toLowerCase().indexOf(this.state.currentName.toLowerCase()) !== -1) && (this.state.currentTitle === "" || person.title_cat === this.state.currentTitle);
         }.bind(this));
-
         this.setState({
           people: filteredPeople
         });
@@ -188,12 +186,7 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
             null,
             "Learn more about each person at Leaf & Mortar in this company directory."
           ),
-          React.createElement(Filters, {
-            currentName: this.state.currentName,
-            currentTitle: this.state.currentTitle,
-            isIntern: this.state.isIntern,
-            updateFormState: this.updateFormState
-          }),
+          React.createElement(Filters, { currentName: this.state.currentName, currentTitle: this.state.currentTitle, isIntern: this.state.isIntern, updateFormState: this.updateFormState }),
           React.createElement(People, { people: this.state.people })
         );
       }
@@ -202,6 +195,6 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
     return Directory;
   }(React.Component);
 
-  ReactDOM.render(React.createElement(Directory, null), document.getElementById("directory-root"));
+  ReactDOM.render(React.createElement(Directory, null), document.getElementById('directory-root'));
 })();
 //# sourceMappingURL=directory-dist.js.map
